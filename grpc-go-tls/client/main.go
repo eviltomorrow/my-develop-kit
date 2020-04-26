a6662420886817df388922892cb55985b4dc5e3f
Captain <eviltomorrow@163.com>
2020-04-26 13:38:13 +0800

    Update main.go

--- grpc-go-tls/client/main.go
+++ grpc-go-tls/client/main.go
@@ -14,17 +14,39 @@ import (
 
 func main() {
 
-	var certFile = "/home/shepard/workspace-agent/project-go/src/github.com/eviltomorrow/grpc-go-tls/certs/ca.crt"
+	
+	var caCertFile = "/home/shepard/workspace-agent/project-go/src/github.com/eviltomorrow/grpc-go-tls/certs/ca.crt"
+	var clientCertFile = "/home/shepard/workspace-agent/project-go/src/github.com/eviltomorrow/grpc-go-tls/certs/client.crt"
+	var clientCertKey = "/home/shepard/workspace-agent/project-go/src/github.com/eviltomorrow/grpc-go-tls/certs/client.pem"
 	var serverName = "localhost"
 
-	// Create tls based credential.
-	creds, err := credentials.NewClientTLSFromFile(testdata.Path(certFile), serverName)
+	// Load the certificates from disk
+	certificate, err := tls.LoadX509KeyPair(clientCertFile, clientCertKey)
 	if err != nil {
-		log.Fatalf("failed to load credentials: %v", err)
+		log.Fatalf("LoadX509KeyPair failure: %v", err)
 	}
 
+	// Create a certificate pool from the certificate authority
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caCertFile)
+	if err != nil {
+		log.Fatalf("Load caCertFile failure: %v", err)
+	}
+
+	// Append the client certificates from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("AppendCertsFromPEM failure: %v", err)
+	}
+
+	// Create the TLS credentials for transport
+	creds := credentials.NewTLS(&tls.Config{
+		ServerName:   serverName,
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      certPool,
+	})
+
 	// Set up a connection to the server.
-	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(creds))
+	conn, err := grpc.Dial("127.0.0.1:8080", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
