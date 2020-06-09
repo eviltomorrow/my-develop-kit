048a80935e4776018fc963b5f4a16d13afe60e0c
shepard <eviltomorrow@163.com>
2020-06-09 12:52:38 +0800

    dev

--- reflect-tool/reflect.go
+++ reflect-tool/reflect.go
@@ -1,8 +1,10 @@
 package tool
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -61,321 +63,94 @@ func find(tag []string, pt reflect.Type, pv reflect.Value) (rt reflect.Type, rv 
 	return rt, rv, fmt.Errorf("Not found specified object with tag[%v]", tag[0])
 }
 
-// SetString set string
-func SetString(key string, value string, object interface{}) error {
+// Set set
+func Set(key string, value interface{}, object interface{}) error {
 	var loc = strings.Split(key, ".")
 	rt, rv, err := FindObjectByTag(loc, object)
 	if err != nil {
 		return err
 	}
 	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
+		return fmt.Errorf("Object value not support modified")
 	}
-	if rt.Kind() != reflect.String {
-		return fmt.Errorf("Not a string type")
-	}
-	rv.SetString(value)
-	return nil
-}
 
-// SetInt64 set int64
-func SetInt64(key string, value int64, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
+	if rt.Kind() != reflect.ValueOf(value).Kind() {
+		return fmt.Errorf("Type not match [expect: %v, actual: %v]", rt.Kind().String(), reflect.ValueOf(value).Kind())
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Int64 {
-		return fmt.Errorf("Not a int64 type")
-	}
-	rv.SetInt(value)
-	return nil
-}
 
-// SetInt32 set int32
-func SetInt32(key string, value int32, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Int32 {
-		return fmt.Errorf("Not a int32 type")
-	}
-	rv.SetInt(int64(value))
-	return nil
-}
-
-// SetInt16 set int16
-func SetInt16(key string, value int32, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Int16 {
-		return fmt.Errorf("Not a int16 type")
-	}
-	rv.SetInt(int64(value))
-	return nil
-}
-
-// SetInt8 set int8
-func SetInt8(key string, value int8, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Int8 {
-		return fmt.Errorf("Not a int8 type")
-	}
-	rv.SetInt(int64(value))
-	return nil
-}
-
-// SetInt set int
-func SetInt(key string, value int, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Int {
-		return fmt.Errorf("Not a int type")
-	}
-	rv.SetInt(int64(value))
-	return nil
-}
-
-// SetFloat64 set float64
-func SetFloat64(key string, value float64, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Float64 {
-		return fmt.Errorf("Not a float64 type")
-	}
-	rv.SetFloat(float64(value))
-	return nil
-}
-
-// SetFloat32 set float32
-func SetFloat32(key string, value float32, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Float32 {
-		return fmt.Errorf("Not a float32 type")
-	}
-	rv.SetFloat(float64(value))
-	return nil
-}
-
-// SetInterface set interface
-func SetInterface(key string, value interface{}, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-	if rt.Kind() != reflect.Interface {
-		return fmt.Errorf("Not a interface type")
+	if !reflect.TypeOf(value).AssignableTo(rt) {
+		return fmt.Errorf("Assignable failure [expect: %v, actual: %v]", rt, reflect.TypeOf(value))
 	}
 	rv.Set(reflect.ValueOf(value))
 	return nil
 }
 
-// SetSliceString set slice string
-func SetSliceString(key string, value []string, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcInt(s string) (interface{}, error) {
+	result, err := strconv.Atoi(s)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.String {
-		return fmt.Errorf("Not a slice string type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return result, nil
 }
 
-// SetSliceInt64 set slice int64
-func SetSliceInt64(key string, value []int64, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcInt32(s string) (interface{}, error) {
+	result, err := strconv.ParseInt(s, 10, 32)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Int64 {
-		return fmt.Errorf("Not a slice int64 type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return int32(result), nil
 }
 
-// SetSliceInt32 set slice int32
-func SetSliceInt32(key string, value []int32, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcInt64(s string) (interface{}, error) {
+	result, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Int32 {
-		return fmt.Errorf("Not a slice int32 type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return result, nil
 }
 
-// SetSliceInt16 set slice int16
-func SetSliceInt16(key string, value []int16, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcFloat32(s string) (interface{}, error) {
+	result, err := strconv.ParseFloat(s, 32)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Int16 {
-		return fmt.Errorf("Not a slice int16 type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return result, nil
 }
 
-// SetSliceInt8 set slice int8
-func SetSliceInt8(key string, value []int8, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcFloat64(s string) (interface{}, error) {
+	result, err := strconv.ParseFloat(s, 64)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Int8 {
-		return fmt.Errorf("Not a slice int8 type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return result, nil
 }
 
-// SetSliceInt set slice int
-func SetSliceInt(key string, value []int, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
-	if err != nil {
-		return err
-	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Int {
-		return fmt.Errorf("Not a slice int type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+func funcString(s string) (interface{}, error) {
+	return s, nil
 }
 
-// SetSliceFloat64 set slice float64
-func SetSliceFloat64(key string, value []float64, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcBool(s string) (interface{}, error) {
+	result, err := strconv.ParseBool(s)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Float64 {
-		return fmt.Errorf("Not a slice float64 type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return result, nil
 }
 
-// SetSliceFloat32 set slice float32
-func SetSliceFloat32(key string, value []float32, object interface{}) error {
-	var loc = strings.Split(key, ".")
-	rt, rv, err := FindObjectByTag(loc, object)
+func funcMapStringAndString(s string) (interface{}, error) {
+	var result map[string]string
+	err := json.Unmarshal([]byte(s), &result)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if !rv.CanSet() {
-		return fmt.Errorf("Object not support modify")
-	}
-
-	if rt.Kind() != reflect.Slice && rt.Elem().Kind() != reflect.Float32 {
-		return fmt.Errorf("Not a slice float32 type")
-	}
-
-	rv.Set(reflect.ValueOf(value))
-	return nil
+	return result, nil
 }
 
-// // SetMap set map
-// func SetMap(key string, k interface{}, v interface{}, object interface{}) error {
-// 	var loc = strings.Split(key, ".")
-// 	rt, rv, err := FindObjectByTag(loc, object)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if !rv.CanSet() {
-// 		return fmt.Errorf("Object not support modify")
-// 	}
-
-// 	return nil
-// }
+func funcSliceString(s string) (interface{}, error) {
+	var result []string
+	err := json.Unmarshal([]byte(s), &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
--- reflect-tool/reflect_test.go
+++ reflect-tool/reflect_test.go
@@ -10,24 +10,12 @@ func TestFindObejctByTag(t *testing.T) {
 			Stocks: 20000,
 			Houses: 3,
 		},
+		Sports: make(map[string]int, 10),
 	}
 
 	rt, rv, err := FindObjectByTag([]string{"asset", "stocks"}, &p)
 	t.Logf("error: %v", err)
 	t.Logf("rt: %v", rt)
 	t.Logf("rv: %v, Canset: %v", rv, rv.CanSet())
-
-	err = SetString("name", "captain", &p)
-	t.Logf("SetString: %v, value: %s", err, p.String())
-
-	err = SetInt64("asset.houses", 100000000, &p)
-	t.Logf("SetInt64: %v, value: %s", err, p.String())
-
-	rt, rv, err = FindObjectByTag([]string{"child"}, &p)
-	t.Logf("error: %v", err)
-	t.Logf("rt: %v", rt)
-	t.Logf("rv: %v, Canset: %v", rv, rv.CanSet())
-	SetSliceString("child", []string{"1", "b"}, &p)
-	t.Logf("SetSliceString: %v, value: %s", err, p.String())
 
 }
